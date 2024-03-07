293b02c9abde82ae65cd49e6e324dbc6920508ce
Eduardo Castro <edusilva.dev@gmail.com>
2024-03-07 11:33:26 -0300

    setup: inicia o front e adiciona regras do eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,59 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime'
+	],
+	'overrides': [
+		{
+			'env': {
+				'node': true
+			},
+			'files': [
+				'.eslintrc.{js,cjs}'
+			],
+			'parserOptions': {
+				'sourceType': 'script'
+			}
+		}
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'@typescript-eslint',
+		'react'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+		'no-multi-spaces': [
+			'error'
+		],
+		'no-multiple-empty-lines': [
+			2, 
+			{'max': 1, 'maxBOF': 0}
+		]
+	}
+};
